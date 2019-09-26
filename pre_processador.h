5cbe6c9caaa610fbb8a8376a40b67c88d947a2dd
Victor Almeida <victor@Victor>
2019-09-26 14:04:10 -0300

    Teste

--- pre_processador.h
+++ pre_processador.h
@@ -4,28 +4,28 @@ std::vector<std::string> directive_placer(std::string code_vector);
 
 
 std::vector<std::string> file_reader(std::string input_file_name){
-		
+
 		std::ifstream input_file (input_file_name);
 		std::vector<std::string> word;
 		char c;
 		std::vector<char> word_c;
 		char word_aux[50];
 		unsigned i, j, k;
-		
+
 		//Rotina para salvar o código em um vetor de caracteres ignorando os comentátios
 		if(input_file.is_open()){
 			while (input_file.get(c)){
 				if (c == ';'){
 					while(c != '\n'){
 						input_file.get(c);
-					};					
+					};
 					word_c.push_back(c);
 				}
 				else {
 					word_c.push_back(c);
-				};	
+				};
 			};
-		
+
 		//Rotina para salvar o código em um vetor de strings ignorando espaços e quebras de linha
 			k = 0;
 			for(i = 0; i < word_c.size(); i++){
@@ -38,7 +38,7 @@ std::vector<std::string> file_reader(std::string input_file_name){
 					};
 					word.push_back(word_aux);
 				};
-			
+
 				for(j=0;j<sizeof(word_aux);j++){
 					word_aux[j] = '\0';
 				};
@@ -48,6 +48,6 @@ std::vector<std::string> file_reader(std::string input_file_name){
 		else{
 			printf("Falha ao abrir arquivo. Verifique se digitou o nome correto e se o arquivo está no mesmo diretório do programa.");
 		}
-		
+
 		return word;
 }
